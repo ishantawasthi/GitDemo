79c0d9d8da5974524fb2779f5a102a7ffd96596b
ishantawasthi <126705668+ishantawasthi@users.noreply.github.com>
2024-04-29 23:46:58 -0600

    Basics

---
+++ TakingInput.java
@@ -0,0 +1,17 @@
+package Basics;
+
+import java.util.*;
+public class TakingInput {
+  public static void main(String[] args) {
+//        Scanner sc =new Scanner(System.in);
+//        System.out.println("Enter the value of x");
+//        int x= sc.nextInt();
+//        System.out.println("Enter the value of y");
+//        int y=sc.nextInt();
+//        System.out.println("The sum of x and y is : "+(x+y));
+      for(int i=0;i<300;i++){
+          System.out.println();
+      }
+
+    }
+}
